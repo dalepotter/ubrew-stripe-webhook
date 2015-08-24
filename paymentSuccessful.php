b1678f83756444d6739f75fa3e009cbaf48fdc84
Dale Potter <dalepotter@gmail.com>
2015-08-25 00:28:00 +0100

    Added logging for each location

--- paymentSuccessful.php
+++ paymentSuccessful.php
@@ -13,23 +13,35 @@ require 'vendor/autoload.php';
 // Include the Stripe config file
 require 'configStripe.php';
 
+// Require helper functions
+require 'functions.php';
+
 // Set Stripe API key
 \Stripe\Stripe::setApiKey(stripeApiKey); // 'stripeApiKey' is defined in configStripe.php
 
-// Read current number of memberships
-$numberMembershipsGreenwich = file_get_contents('greenwichNumber.txt');
-
 // Process the data from the webhook
 $body = @file_get_contents('php://input');
-$eventJson = json_decode($body);
+$body = preg_replace('#\[\s+#', '[', $body);
+$eventJson = json_decode(utf8_encode($body), true);
+
+// For extra security, retrieve from the Stripe API
+$event_id = $eventJson['id'];
+$event = \Stripe\Event::retrieve($event_id);
 
 // Store transaction data in 'data.txt' (This overwrites any existing data in the file)
 file_put_contents('data.txt', $body);
+file_put_contents('data1.txt', $eventJson['id']);
 
-// Increment the number of memberships for Greenwich
-$newNumberMembershipsGreenwich = $numberMembershipsGreenwich + 1;
+// Increment the number of memberships for either Greenwich or Dalston depending on the description
+  if (find_in_string('Greenwich', $event->data->object['description'])){
+     $numberMembershipsGreenwich = file_get_contents('greenwichNumber.txt');
+     $newNumberMembershipsGreenwich = $numberMembershipsGreenwich + 1;
+     file_put_contents('greenwichNumber.txt', $newNumberMembershipsGreenwich);
+  } elseif (find_in_string('Dalston', $event->data->object['description'])){
+     $numberMembershipsDalston = file_get_contents('dalstonNumber.txt');
+     $newNumberMembershipsDalston = $numberMembershipsDalson + 1;
+     file_put_contents('dalstonNumber.txt', $newNumberMembershipsDalston);
+  }
 
-// Write this new number to the file
-file_put_contents('greenwichNumber.txt', $newNumberMembershipsGreenwich);
 ?>
 
